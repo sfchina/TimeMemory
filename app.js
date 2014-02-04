979b7ae01f664fb674ffdeb56787c6d92a13a36b
sfchina <sfchina@sina.com>
2014-02-05 00:39:08 +0900

    some code to store

--- app.js
+++ app.js
@@ -4,10 +4,24 @@
  * MIT Licensed
  */
 var express=require('express');
+var router=require('./router');
 var app=express();
+var conf=require('./conf');
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+global.conf=conf;
+
+app.set('port', process.env.PORT || 5050);
+
+app.use(function(req, res, next){
+	  console.log('%s %s', req.method, req.url);
+	  next();
 });
+app.use(express.favicon());
+app.use(express.static(__dirname + '/public'));
+app.use(express.logger());
+ 
+app.get('/serv/:serv?',router.serv);
 
-app.listen(5050);
+app.listen(app.get('port'),function(){
+  console.log('Time Memory server listening on port '+app.get('port'));
+});
